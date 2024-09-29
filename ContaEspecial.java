28fa2c4c33459ba019068af94e51a9b8eff3e979
Pedro <delarcinavick@gmail.com>
2024-09-29 20:09:25 -0300

    Primeiro commit - adicionando projeto

---
+++ ContaEspecial.java
@@ -0,0 +1,18 @@
+public class ContaEspecial extends Conta {
+    private double limite;
+
+    public ContaEspecial(double limite) {
+        super();
+        this.limite = limite;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        if (valor > 0 && (valor <= saldo + limite)) {
+            saldo -= valor;
+            System.out.println("Saque de R$ " + valor + " realizado.");
+        } else {
+            System.out.println("Saque inválido. Saldo atual: R$ " + saldo + ", limite: R$ " + limite);
+        }
+    }
+}
